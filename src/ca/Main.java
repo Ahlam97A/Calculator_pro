282944450ab2d050ebb9b457cf6bf873ef00252e
Ahlam <ahlamkharrousheh@hotmail.com>
2018-02-04 09:23:56 -0800

    calculator project

---
+++ src/ca/Main.java
@@ -0,0 +1,11 @@
+package ca;
+
+public class Main {
+
+	public static final boolean DEBUG_MODE = true;
+
+	public static void main(String[] args) {
+		Controller app = new Controller();
+		app.run();
+	}
+}
